878d1877387f5d63457f0fb229007f98ef02be14
Billie Delos Santos <billiedelossantos@actimai.com>
2025-01-18 12:12:32 +0800

    updated js

--- js/script_primer.js
+++ js/script_primer.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 window.watsonAssistantChatOptions = {
-  integrationID: "c6cd805b-092d-4ae9-9962-2a0e3f852d18", // The ID of this integration.
+  integrationID: "f3e4b4ca-b64d-4f2a-98f0-6cc7434ee883", // The ID of this integration.
   region: "us-south", // The region your integration is hosted in.
   serviceInstanceID: "a698c434-eb00-4f89-a50c-fb2fb4181554", // The ID of your service instance.
   onLoad: async (instance) => { await instance.render(); },
